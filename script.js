31f704caedcc6230b00a7c531d6e0d2b52ded5e5
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-02-21 17:33:22 +1100

    change event listener to parent div
    use event delegation to reduce event listeners

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const buttons = document.querySelectorAll("li");
+//const buttons = document.querySelectorAll("li");
+const buttons1 = document.querySelector(".numPad");
 const display = document.querySelector(".numDisplay");
 
-buttons.forEach((button) => {
-    button.addEventListener("click", (e) => { 
-        press(e.target);
-    })
-});
+let displayVal = 0;
+
+//buttons1.addEventListener("click", press(this.target));
+ buttons1.addEventListener("click", (e) => { 
+     press(e.target);
+ });
+
+
+// buttons.forEach((button) => {
+//     button.addEventListener("click", (e) => { 
+//         //press(e.target);
+//     })
+// });
 
 
 function press(target) {
     console.log(target.textContent);
-
     if (target.textContent == "AC") clear();
+
+
+    
 }
+
 
 function clear() {
     display.textContent = 0;
