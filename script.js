d6d8ff5b5cdfb2b6897e07b041f9f910d6eee9f0
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-02-21 18:44:27 +1100

    highlight selected operator
    highlight selected operator and clear when clear function runs

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-//const buttons = document.querySelectorAll("li");
-const buttons1 = document.querySelector(".numPad");
+const buttons = document.querySelectorAll("li");
+const numPad = document.querySelector(".numPad");
 const display = document.querySelector(".numDisplay");
 
 let displayVal = "0";
 let newVal = "";
 let operating = 0;
+let operator = "";
 
 //buttons1.addEventListener("click", press(this.target));
- buttons1.addEventListener("click", (e) => { 
+ numPad.addEventListener("click", (e) => { 
      press(e.target);
  });
 
@@ -40,8 +41,21 @@ function press(target) {
         val == "-" ||
         val == "+" ||
         val == "÷"
-    )  operate(target);
+    )  operate(val);
 
+ 
+    
+    if (operating == 1 && (
+        val == "x" ||
+        val == "-" ||
+        val == "+" ||
+        val == "÷")) {
+            buttons[3].style.backgroundColor = "orange";
+            buttons[7].style.backgroundColor = "orange";
+            buttons[11].style.backgroundColor = "orange";
+            buttons[15].style.backgroundColor = "orange";
+            target.style.backgroundColor = "red";
+        }
 
     if (operating == 1 && newVal != "") display.textContent = newVal;
     else display.textContent = displayVal;
@@ -52,6 +66,10 @@ function clear() {
     displayVal = "0";
     newVal = "";
     operating = 0;
+    buttons[3].style.backgroundColor = "orange";
+    buttons[7].style.backgroundColor = "orange";
+    buttons[11].style.backgroundColor = "orange";
+    buttons[15].style.backgroundColor = "orange";
 }
 
 function addNum(val) {
@@ -69,8 +87,9 @@ function addNum(val) {
       
 }
 
-function operate(target) {
+function operate(val) {
     operating = 1;
+    operator = val;
     //target.style.backgroundColor = "red";
 
     // if (val == "x") console.log("Times!");
