290610bc5c21f6b9c5a91ecd6546dba2b01c82a2
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-02-21 17:44:14 +1100

    add event listener to update display
    add number to display when a numbered button is pressed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const buttons1 = document.querySelector(".numPad");
 const display = document.querySelector(".numDisplay");
 
-let displayVal = 0;
+let displayVal = "0";
 
 //buttons1.addEventListener("click", press(this.target));
  buttons1.addEventListener("click", (e) => { 
@@ -18,14 +18,32 @@ let displayVal = 0;
 
 
 function press(target) {
-    console.log(target.textContent);
-    if (target.textContent == "AC") clear();
+    const val = target.textContent;
+    if (val == "AC") clear();
+    if (val == "0" ||
+        val == "1" ||
+        val == "2" ||
+        val == "3" ||
+        val == "4" ||
+        val == "5" ||
+        val == "6" ||
+        val == "7" ||
+        val == "8" ||
+        val == "9"
+    ) addNum(val);
 
 
-    
+    display.textContent = displayVal;
 }
 
 
 function clear() {
-    display.textContent = 0;
+    displayVal = "0";
+}
+
+function addNum(val) {
+    if (displayVal == "0") {
+        displayVal = val;
+    }
+    else displayVal += val;
 }
